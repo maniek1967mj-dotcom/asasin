6a35bc24662cc81797f2d89a47f7678c28e36699
maniek1967mj-dotcom <maniek1967.mj@gmail.com>
2025-10-01 10:52:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -219,3 +219,6 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     print(f"Starting Flask app on port {port}...", flush=True)
     app.run(host='0.0.0.0', port=port, debug=False)
+else:
+    # Gdy uruchamiany przez Gunicorn
+    print(f"Flask app ready for Gunicorn", flush=True)
